2c2d3312650b9bc6880ea5fb8175a42dc7aa56ce
Glitch (rust-workable-kettledrum) <none>
2020-09-20 21:56:47 +0000

    🐄⛩ Checkpoint ./scripts/addIcon.js:22671151/867

--- scripts/addIcon.js
+++ scripts/addIcon.js
@@ -1,10 +1,11 @@
 const colors = require('colors/safe');
 const fs = require('fs');
+const mime = require('mime-types');
 const manifestJS = require('./manifest.js');
 const dir = process.cwd();
 const fields = [
     new manifestJS.field("src", "src (Source path to image, default: src/img/image.png)", "src/img/image.png"),
-    new manifestJS.field("type", "type (Type of icon file, either mime or some extensions, default: png)", "png"),
+    new manifestJS.field("type", "type (Type of icon file, either mime or auto, default: auto)", "auto"),
     new manifestJS.field("sizes", "sizes (Size of icon, recommended: 192x192, default: 192x192)", "192x192")
 ]
 let count = 0;
@@ -33,6 +34,15 @@ function run(rl) {
             }
             else {
               obj[fields[count].name] = answer;
+            }
+            if (count === 1 && answer.toLowerCase().trim() === "auto") {
+              let detectedMime = mime.lookup(obj['src']);
+              if (detectedMime === false) {
+                console.log('Unable to detect mime type from src path, please make sure your src leads to a valid icon file.');
+              }
+              else {
+                obj[fields[count].name] = detectedMime;
+              }
             }
             console.log(obj);
             console.log("\t");
